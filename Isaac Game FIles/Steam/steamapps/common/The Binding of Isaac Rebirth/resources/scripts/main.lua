8f1f816791a89aa419a62cda17a2be9d9033b34b
CStaplesLewis <douglastylerbowen@gmail.com>
2017-01-05 07:58:37 -0600

    Adding unpacked resources

---
+++ Isaac Game FIles/Steam/steamapps/common/The Binding of Isaac Rebirth/resources/scripts/main.lua
@@ -0,0 +1,24 @@
+function RegisterMod(modname, apiversion)
+  local mod = {
+    Name = modname,
+    AddCallback = function(self, callbackId, fn, entityId)
+      if entityId == nil then entityId = -1; end
+      
+      Isaac.AddCallback(self, callbackId, fn, entityId)
+    end,
+    SaveData = function(self, data)
+      Isaac.SaveModData(self, data)
+    end,
+    LoadData = function(self)
+      return Isaac.LoadModData(self)
+    end,
+    HasData = function(self)
+      return Isaac.HasModData(self)
+    end,
+    RemoveData = function(self)
+      Isaac.RemoveModData(self)
+    end
+  }
+  Isaac.RegisterMod(mod, modname, apiversion)
+  return mod
+end
