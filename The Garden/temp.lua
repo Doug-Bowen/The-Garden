eb1d885d683b86593fbe5e967af87970a867e468
CStaplesLewis <doug.bowen@flowcal.com>
2017-01-06 09:52:08 -0600

    Add baseline LUA helper to The Garden Mod

---
+++ The Garden/temp.lua
@@ -0,0 +1,32 @@
+require('mobdebug').start()
+
+local myMod = RegisterMod("hello-mod", 1)
+
+function myMod:PlayerInit(aConstPlayer)
+end
+
+function myMod:Text()
+end
+
+function myMod:TakeDamage(aEntity)
+end
+
+function myMod:PostUpdate()
+end
+
+function myMod:PostPerfectUpdate(aConstPlayer)
+end
+
+function myMod:NpcUpdate(aNpc)
+end
+
+function myMod:PostRender()
+end
+
+myMod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, myMod.PlayerInit)
+myMod:AddCallback(ModCallbacks.MC_POST_RENDER, myMod.Text)
+myMod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, myMod.TakeDamage)
+myMod:AddCallback(ModCallbacks.MC_POST_UPDATE, myMod.PostUpdate)
+myMod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, myMod.PostPerfectUpdate)
+myMod:AddCallback(ModCallbacks.MC_POST_RENDER, myMod.PostRender)
+myMod:AddCallback(ModCallbacks.MC_NPC_UPDATE, myMod.NpcUpdate)
