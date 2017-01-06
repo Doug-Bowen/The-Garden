36e588adca28a8ecce453cc29ca2a278379ec57f
CStaplesLewis <doug.bowen@flowcal.com>
2017-01-06 15:58:26 -0600

    Update shameEffect()

--- The Garden/resources/scripts/main.lua
+++ The Garden/resources/scripts/main.lua
@@ -2,7 +2,7 @@ require('mobdebug').start()
 
 local debugFile = io.open("Log-TheGarden.txt", "w")
 
-local theGardenMod = RegisterMod("TheGarden", 1)
+local theGardenMod = RegisterMod("TheGarden", 1) --'1' denotes API v1.0
 local shameItem = Isaac.GetItemIdByName("Shame")
 local forbiddenFruitItem = Isaac.GetItemIdByName("Forbidden Fruit")
 local deceptionItem = Isaac.GetItemIdByName("Deception")
@@ -14,7 +14,7 @@ local exiledItem = Isaac.GetItemIdByName("Exiled")
 local miracleGrowItem = Isaac.GetItemIdByName("MiracleGrow")
 local genesisChallengeId = Isaac.GetChallengeIdByName("Genesis");
 
-function useShameItem()
+function shameEffect()
 	local player = Isaac.GetPlayer(0);	
 	local playerPosition = Isaac.GetFreeNearPosition(player.Position, 1);
 	local entities = Isaac.GetRoomEntities()
@@ -28,4 +28,4 @@ function useShameItem()
 	end
 end
 
-mod:AddCallback(ModCallbacks.MC_USE_ITEM, useShameItem, shameItem);
+mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE , shameEffect, shameItem); --I believe this call back might be incorrect. TODO: Find the correct call back
