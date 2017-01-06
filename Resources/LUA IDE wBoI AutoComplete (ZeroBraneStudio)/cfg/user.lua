b1702174040c92cefaac5c337553115a70a0ba6f
CStaplesLewis <doug.bowen@flowcal.com>
2017-01-06 09:51:53 -0600

    Add LUA temp settings

---
+++ Resources/LUA IDE wBoI AutoComplete (ZeroBraneStudio)/cfg/user.lua
@@ -0,0 +1,4 @@
+--[[--
+  Use this file to specify **System** preferences.
+  Review [examples](+F:\isaacMods\Resources\LUA IDE wBoI AutoComplete (ZeroBraneStudio)\cfg\user-sample.lua) or check [online documentation](http://studio.zerobrane.com/documentation.html) for details.
+--]]--
