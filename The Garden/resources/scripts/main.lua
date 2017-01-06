91b9549f3d9fd85bb3af852ea8efe14c83d7b603
CStaplesLewis <doug.bowen@flowcal.com>
2017-01-06 13:55:01 -0600

    rebuild main.lua

--- The Garden/resources/scripts/main.lua
+++ The Garden/resources/scripts/main.lua
@@ -2,42 +2,22 @@ require('mobdebug').start()
 
 local debugFile = io.open("Log-TheGarden.txt", "w")
 
-local theGarden = RegisterMod("TheGarden", 1)
-local shame = Isaac.GetItemIdByName("Shame")
-local forbiddenFruit = Isaac.GetItemIdByName("Forbidden Fruit")
-local deception = Isaac.GetItemIdByName("Deception")
-local grantedDomain = Isaac.GetItemIdByName("Granted Domain")
-local theWillOfMan = Isaac.GetItemIdByName("The Will of Man")
-local theFallOfMan = Isaac.GetItemIdByName("The Fall of Man")
-local rebirth = Isaac.GetItemIdByName("Rebirth")
-local exiled = Isaac.GetItemIdByName("Exiled")
-local miracleGrow = Isaac.GetItemIdByName("MiracleGrow")
+local theGardenMod = RegisterMod("TheGarden", 1)
+local shameItem = Isaac.GetItemIdByName("Shame")
+local forbiddenFruitItem = Isaac.GetItemIdByName("Forbidden Fruit")
+local deceptionItem = Isaac.GetItemIdByName("Deception")
+local grantedDomainItem = Isaac.GetItemIdByName("Granted Domain")
+local theWillOfManItem = Isaac.GetItemIdByName("The Will of Man")
+local theFallOfManItem = Isaac.GetItemIdByName("The Fall of Man")
+local rebirthItem = Isaac.GetItemIdByName("Rebirth")
+local exiledItem = Isaac.GetItemIdByName("Exiled")
+local miracleGrowItem = Isaac.GetItemIdByName("MiracleGrow")
 
-function myMod:PlayerInit(aConstPlayer)
+function useShameItem()
+--Placeholder code
+	local player = Isaac.GetPlayer(0);	
+	local pos = Isaac.GetFreeNearPosition(player.Position, 1);
+	Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, debuggingItem, pos, pos, player);
 end
 
-function myMod:Text()
-end
-
-function myMod:TakeDamage(aEntity)
-end
-
-function myMod:PostUpdate()
-end
-
-function myMod:PostPerfectUpdate(aConstPlayer)
-end
-
-function myMod:NpcUpdate(aNpc)
-end
-
-function myMod:PostRender()
-end
-
-myMod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, myMod.PlayerInit)
-myMod:AddCallback(ModCallbacks.MC_POST_RENDER, myMod.Text)
-myMod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, myMod.TakeDamage)
-myMod:AddCallback(ModCallbacks.MC_POST_UPDATE, myMod.PostUpdate)
-myMod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, myMod.PostPerfectUpdate)
-myMod:AddCallback(ModCallbacks.MC_POST_RENDER, myMod.PostRender)
-myMod:AddCallback(ModCallbacks.MC_NPC_UPDATE, myMod.NpcUpdate)
+mod:AddCallback(ModCallbacks.MC_USE_ITEM, useShameItem, shameItem);
