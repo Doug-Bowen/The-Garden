0ddf433e2b69aaa403304eb4be1a8af5422bc7a3
CStaplesLewis <douglastylerbowen@gmail.com>
2017-01-06 22:28:41 -0600

    Update lua (working on makeing Shame work

--- The Garden/main.lua
+++ The Garden/main.lua
@@ -1,8 +1,7 @@
-require('mobdebug').start()
+--require('mobdebug').start()
+--local debugFile = io.open("Log-TheGarden.txt", "w")
+local garden = RegisterMod("TheGarden", 1) --'1' denotes API v1.0
 
-local debugFile = io.open("Log-TheGarden.txt", "w")
-
-local theGardenMod = RegisterMod("TheGarden", 1) --'1' denotes API v1.0
 local shameItem = Isaac.GetItemIdByName("Shame")
 local forbiddenFruitItem = Isaac.GetItemIdByName("Forbidden Fruit")
 local deceptionItem = Isaac.GetItemIdByName("Deception")
@@ -12,20 +11,21 @@ local theFallOfManItem = Isaac.GetItemIdByName("The Fall of Man")
 local rebirthItem = Isaac.GetItemIdByName("Rebirth")
 local exiledItem = Isaac.GetItemIdByName("Exiled")
 local miracleGrowItem = Isaac.GetItemIdByName("MiracleGrow")
-local genesisChallengeId = Isaac.GetChallengeIdByName("Genesis");
+local genesisChallengeId = Isaac.GetChallengeIdByName("Genesis")
 
-function shameEffect()
-	local player = Isaac.GetPlayer(0);	
-	local playerPosition = Isaac.GetFreeNearPosition(player.Position, 1);
-	local entities = Isaac.GetRoomEntities()
-	for i = 1, #entities do
-		if entities[i]:IsVulnerableEnemy() then
-			if entities[i]:TargetPosition() == playerPosition then				
-				entities[i]:AddFear(EntityRef(player), 120)		
-			end
+function garden:shameEffect()
+	local player = Isaac.GetPlayer(0)
+	if player.HasCollectible(shameItem) then
+		local playerPosition = Isaac.GetFreeNearPosition(player.Position, 1)
+		local entities = Isaac.GetRoomEntities()
+		for i = 1, #entities do
+			--if entities[i]:IsVulnerableEnemy() then
+				--if entities[i]:TargetPosition() == playerPosition then				
+					entities[i]:AddFear(EntityRef(player), 120)		
+				--end
+			--end
 		end
-	Isaac.DebugString("[DEBUG FLAG] You used Shame")		
 	end
 end
 
-mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE , shameEffect, shameItem); --I believe this call back might be incorrect. TODO: Find the correct call back
+garden:AddCallback(ModCallbacks.MC_POST_UPDATE, garden.shameEffect) --I believe this it an incorrect Mod Callback
