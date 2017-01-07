030bc7d08d53c795445d4fed4c494771467b9455
CStaplesLewis <douglastylerbowen@gmail.com>
2017-01-07 08:43:11 -0600

    SHAME WORKS!

--- The Garden/main.lua
+++ The Garden/main.lua
@@ -1,8 +1,7 @@
---require('mobdebug').start()
+StartDebug()
 --local debugFile = io.open("Log-TheGarden.txt", "w")
 local garden = RegisterMod("TheGarden", 1) --'1' denotes API v1.0
 
-local shameItem = Isaac.GetItemIdByName("Shame")
 local forbiddenFruitItem = Isaac.GetItemIdByName("Forbidden Fruit")
 local deceptionItem = Isaac.GetItemIdByName("Deception")
 local grantedDomainItem = Isaac.GetItemIdByName("Granted Domain")
@@ -15,17 +14,26 @@ local genesisChallengeId = Isaac.GetChallengeIdByName("Genesis")
 
 function garden:shameEffect()
 	local player = Isaac.GetPlayer(0)
-	if player.HasCollectible(shameItem) then
-		local playerPosition = Isaac.GetFreeNearPosition(player.Position, 1)
+	local shameItem = Isaac.GetItemIdByName("Shame")
+	if player:HasCollectible(shameItem) then
 		local entities = Isaac.GetRoomEntities()
 		for i = 1, #entities do
-			--if entities[i]:IsVulnerableEnemy() then
-				--if entities[i]:TargetPosition() == playerPosition then				
-					entities[i]:AddFear(EntityRef(player), 120)		
-				--end
-			--end
+			local singleEntity = entities[i]
+			if singleEntity:IsVulnerableEnemy() then
+				--Isaac.RenderText("Testing Shame", 50, 15, 255, 255, 255, 255)
+				local playerPosition = player.Position
+				local entityPosition = singleEntity.Position
+				local playerXCoord = playerPosition.X
+				local playerYCoord = playerPosition.Y
+				local entityXCoord = entityPosition.X
+				local entityYCoord = entityPosition.Y
+				local positionalDifference = Vector(playerXCoord-entityXCoord,playerYCoord-entityYCoord)
+				if math.abs(positionalDifference.X) < 60 and math.abs(positionalDifference.Y) < 60 then
+					singleEntity:AddFear(EntityRef(player), 4)		
+				end
+			end
 		end
 	end
 end
 
-garden:AddCallback(ModCallbacks.MC_POST_UPDATE, garden.shameEffect) --I believe this it an incorrect Mod Callback
+garden:AddCallback(ModCallbacks.MC_POST_UPDATE, garden.shameEffect) --This callback is for checking per frame
