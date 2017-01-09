59e24f82c38ca07c7bf3078aec2b404c9cb1c61b
CStaplesLewis <douglastylerbowen@gmail.com>
2017-01-08 20:30:25 -0600

    continue building the garden room

--- The Garden/main.lua
+++ The Garden/main.lua
@@ -14,11 +14,12 @@ garden.CHALLENGE_GENISIS = Isaac.GetChallengeIdByName("Genesis")
 function garden:shameEffect()
 	local player = Isaac.GetPlayer(0)
 	if player:HasCollectible(garden.COLLECTIBLE_SHAME) then
+		--shameItem = Config:Item:Name("Shame")
+		--Game():GetPlayer(0):AddCostume(shameItem)
 		local entities = Isaac.GetRoomEntities()
 		for i = 1, #entities do
 			local singleEntity = entities[i]
-			if singleEntity:IsVulnerableEnemy() then
-				
+			if singleEntity:IsVulnerableEnemy() then		
 				local playerPosition = player.Position
 				local entityPosition = singleEntity.Position
 				local positionalDifference = Vector(playerPosition.X-entityPosition.X, playerPosition.Y-entityPosition.Y)
@@ -30,19 +31,27 @@ function garden:shameEffect()
 	end
 end
 
-function garden:forbiddenFruitEffect()
-end
-
-function garden:gardenRoom()
+function garden:gardenRoomUpdate()
 	local currentRoom = Game():GetRoom()
 	local roomType = currentRoom:GetType()
-	if(roomType == 12) then --is the room a library
-		Isaac.RenderText("WELCOME TO THE LIBRARY", 50, 15, 255, 255, 255, 255)	
-		--local doorState = currentRoom.DoorState
-		
+	--Isaac.RenderText(roomType, 300, 15, 255, 255, 255, 255)	
+	if(roomType == 0) then --is the garden room
+		--pinHasSpawned = false should be set with this room initializes
+		local player = Isaac.GetPlayer(0)
+		local playerPosition = player.Position
+		local roomCenter = currentRoom:GetCenterPos()
+		local positionalDifference = Vector(playerPosition.X-roomCenter.X, playerPosition.Y-roomCenter.Y)
+		if math.abs(positionalDifference.X) < 20 and math.abs(positionalDifference.Y) < 20 then
+			--Isaac.RenderText("YOU ARE IN THE CENTER", 50, 25, 255, 255, 255, 255)
+			if(pinHasSpawned == false) then
+				Isaac.Spawn(EntityType.ENTITY_PIN, 1, 1, Vector(roomCenter.X, roomCenter.Y+100), Vector(0,0), Isaac.GetPlayer(0));	
+				pinHasSpawned = true
+			end				
+		end
+
+		--local doorState = currentRoom.DoorState		
 	end
 end
 
 garden:AddCallback(ModCallbacks.MC_POST_UPDATE, garden.shameEffect)
-garden:AddCallback(ModCallbacks.MC_POST_UPDATE, garden.forbiddenFruitEffect)
-garden:AddCallback(ModCallbacks.MC_POST_UPDATE, garden.gardenRoom)
+garden:AddCallback(ModCallbacks.MC_POST_UPDATE, garden.gardenRoomUpdate)
