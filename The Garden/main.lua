272deb81e83cb82de7663f8fc4784c393411fc53
CStaplesLewis <doug.bowen@flowcal.com>
2017-01-09 09:34:03 -0600

    Rebuild item pool into object

--- The Garden/main.lua
+++ The Garden/main.lua
@@ -1,15 +1,17 @@
 local garden = RegisterMod("TheGarden", 1) --'1' denotes API v1.0
 
-garden.COLLECTIBLE_SHAME = Isaac.GetItemIdByName("Shame")
-garden.COLLECTIBLE_FORBIDDEN_FRUIT = Isaac.GetItemIdByName("Forbidden Fruit")
-garden.COLLECTIBLE_DECEPTION = Isaac.GetItemIdByName("Deception")
-garden.COLLECTIBLE_GRANTED_DOMAIN = Isaac.GetItemIdByName("Granted Domain")
-garden.COLLECTIBLE_THE_WILL_OF_MAN = Isaac.GetItemIdByName("The Will of Man")
-garden.COLLECTIBLE_THE_FALL_OF_MAN = Isaac.GetItemIdByName("The Fall of Man")
-garden.COLLECTIBLE_REBIRTH = Isaac.GetItemIdByName("Rebirth")
-garden.COLLECTIBLE_EXILED = Isaac.GetItemIdByName("Exiled")
-garden.COLLECTIBLE_MIRACLE_GROW = Isaac.GetItemIdByName("MiracleGrow")
-garden.CHALLENGE_GENISIS = Isaac.GetChallengeIdByName("Genesis")
+local gardenPool = {
+	garden.COLLECTIBLE_SHAME = Isaac.GetItemIdByName("Shame")
+	garden.COLLECTIBLE_FORBIDDEN_FRUIT = Isaac.GetItemIdByName("Forbidden Fruit")
+	garden.COLLECTIBLE_DECEPTION = Isaac.GetItemIdByName("Deception")
+	garden.COLLECTIBLE_GRANTED_DOMAIN = Isaac.GetItemIdByName("Granted Domain")
+	garden.COLLECTIBLE_THE_WILL_OF_MAN = Isaac.GetItemIdByName("The Will of Man")
+	garden.COLLECTIBLE_THE_FALL_OF_MAN = Isaac.GetItemIdByName("The Fall of Man")
+	garden.COLLECTIBLE_REBIRTH = Isaac.GetItemIdByName("Rebirth")
+	garden.COLLECTIBLE_EXILED = Isaac.GetItemIdByName("Exiled")
+	garden.COLLECTIBLE_MIRACLE_GROW = Isaac.GetItemIdByName("MiracleGrow")
+	garden.CHALLENGE_GENISIS = Isaac.GetChallengeIdByName("Genesis")
+}
 
 function garden:shameEffect()
 	local player = Isaac.GetPlayer(0)
